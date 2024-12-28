fd7031902a8434e0d79ca424bf9ef18f9e63ebde
Volter Entoma <volterentoma1@gmail.com>
2024-12-29 00:20:42 +1000

    refactor: remove onViewRangeChanged method and replaced in constructor

--- src/AppController.cpp
+++ src/AppController.cpp
@@ -13,14 +13,12 @@ AppController::AppController()
 
     // Update the plottable sensors in the viewModel from the dataManager buffers
     updatePlottableSensors();
-}
 
-// callback for when the view range changes in the graphView (from the user)
-void AppController::onViewRangeChanged(double start, double end) {
-    // Update the range
-    // For testing, we update the range for sensor_1 and sensor_2
-    dataManager.updateSensorRange("sensor_1", start, end);
-    dataManager.updateSensorRange("sensor_2", start, end);
+    // Set the callback for when the view range changes in the graphView
+    graphView.setUpdateRangeCallback(
+    [this](const std::string& sensor_id, int plot_id, double start, double end) {
+        dataManager.updateSensorRange(sensor_id, plot_id, static_cast<DataManager::Timestamp>(start), static_cast<DataManager::Timestamp>(end));
+    });
 }
 
 // Update the plottable sensors in the viewModel from the dataManager buffers
