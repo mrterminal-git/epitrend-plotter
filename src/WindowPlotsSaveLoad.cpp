4ead28c51cf8a6fd545b257ae44b4605bfae70c1
mr-molly <VolterE@silanna.com>
2025-01-15 13:48:13 +1000

    add: WindowPlotsSaveLoad class

---
+++ src/WindowPlotsSaveLoad.cpp
@@ -0,0 +1,57 @@
+#include "WindowPlotsSaveLoad.hpp"
+#include <fstream>
+
+nlohmann::json WindowPlotsSaveLoad::serialize(const WindowPlots& windowPlots) {
+    nlohmann::json j;
+    j["label"] = windowPlots.getLabel();
+    j["renderable_plots"] = nlohmann::json::array();
+    for (const auto& [label, plot] : windowPlots.getRenderablePlots()) {
+        j["renderable_plots"].push_back({{"label", label}, {"plot", serialize(*plot)}});
+    }
+    return j;
+}
+
+WindowPlots WindowPlotsSaveLoad::deserialize(const nlohmann::json& j) {
+    WindowPlots window(j.at("label").get<std::string>());
+    for (const auto& item : j.at("renderable_plots")) {
+        auto plot = std::make_unique<RenderablePlot>(deserializeRenderablePlot(item.at("plot")));
+        window.addRenderablePlot(item.at("label").get<std::string>(), std::move(plot));
+    }
+    return window;
+}
+
+nlohmann::json WindowPlotsSaveLoad::serialize(const RenderablePlot& renderablePlot) {
+    nlohmann::json j;
+    j["label"] = renderablePlot.getLabel();
+    j["real_time"] = renderablePlot.isRealTime();
+    // Serialize other members...
+    return j;
+}
+
+RenderablePlot WindowPlotsSaveLoad::deserializeRenderablePlot(const nlohmann::json& j) {
+    RenderablePlot plot(j.at("label").get<std::string>(), j.at("real_time").get<bool>());
+    // Deserialize other members...
+    return plot;
+}
+
+void WindowPlotsSaveLoad::saveToFile(const WindowPlots& windowPlots, const std::string& filename) {
+    std::ofstream file(filename);
+    if (file.is_open()) {
+        file << serialize(windowPlots).dump(4); // Pretty print with 4 spaces
+        file.close();
+    } else {
+        throw std::runtime_error("Unable to open file for writing: " + filename);
+    }
+}
+
+WindowPlots WindowPlotsSaveLoad::loadFromFile(const std::string& filename) {
+    std::ifstream file(filename);
+    if (file.is_open()) {
+        nlohmann::json j;
+        file >> j;
+        file.close();
+        return deserialize(j);
+    } else {
+        throw std::runtime_error("Unable to open file for reading: " + filename);
+    }
+}
---
+++ src/WindowPlotsSaveLoad.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+#include <nlohmann/json.hpp>
+#include "WindowPlots.hpp"
+#include "RenderablePlot.hpp"
+
+class WindowPlotsSaveLoad {
+public:
+    static nlohmann::json serialize(const WindowPlots& windowPlots);
+    static WindowPlots deserialize(const nlohmann::json& j);
+
+    static nlohmann::json serialize(const RenderablePlot& renderablePlot);
+    static RenderablePlot deserializeRenderablePlot(const nlohmann::json& j);
+
+    static void saveToFile(const WindowPlots& windowPlots, const std::string& filename);
+    static WindowPlots loadFromFile(const std::string& filename);
+};
+
